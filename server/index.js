c9ae5205c6f722d2f2232caa0b3ae5d83ab116d1
wilsonhou <wilshou@gmail.com>
2021-04-05 13:38:04 +1000

    Set up boilerplate for Express server
    Installed required packages and set up index.js file.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send({
+    data: "Hello world!",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening as port ${port}`);
+});
