6b6bae9a1d41d9950897225a93bb257ef2f50560
wilsonhou <wilshou@gmail.com>
2021-04-09 19:36:25 +1000

    Installed Additional Dependencies
    Set up MongoDB Atlas Cluster. Installed Express middleware.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(cors());
 
 const app = express();
-const port = 2000;
+const port = 2020;
 
 app.get("/", (req, res) => {
   res.send({
