4fa9f404410a3ba59078314f14b3b960e5dcf3b4
wilsonhou <wilshou@gmail.com>
2021-04-16 16:29:52 +1000

    Added Environmental Variables

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const ideasRouter = require("./controllers/ideas");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use("/api", ideasRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://wilsonhou:E6M6Fx7DUDqasr@idea-garden.sjhhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected!"))
   .catch(err => {
     console.log("Error while connecting: " + err.message);
   });
 
-const PORT = 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
